c679dba6b77fb7fb72e372a7e03ba8a48c8830d6
WILGE2004 <wilhelmgeney@gmail.com>
2022-03-28 00:34:43 -0500

    Adiciono vistas y menú

--- js/index.js
+++ js/index.js
@@ -4,25 +4,25 @@ const porcentaje_impuesto = document.formulario.p_impuestos;
 const valor_venta = document.formulario.v_venta;
 const ganancia = document.formulario.ganancia;
 
-const calculate = () => {
-  const compra = valor_compra.value;
-  const utilidad = porcentaje_utilidad.value;
-  const impuesto = porcentaje_impuesto.value;
+function calculate() {
+  const compra = parseInt(valor_compra.value);
+  const utilidad = parseInt(porcentaje_utilidad.value);
+  const impuesto = parseInt(porcentaje_impuesto.value);
 
   const valor_utilidad = (compra * utilidad) / 100;
   const valor_impuestos = (compra * impuesto) / 100;
 
-  const total_venta = valor_utilidad + valor_impuestos + parseInt(compra);
-  const ganancia_venta = total_venta - parseInt(compra) - valor_impuestos;
+  const total_venta = valor_utilidad + valor_impuestos + compra;
+  const ganancia_venta = total_venta - compra - valor_impuestos;
 
   valor_venta.value = total_venta;
   ganancia.value = ganancia_venta;
-};
+}
 
-const delete_info = () => {
+function delete_info() {
   valor_compra.value = 0;
   porcentaje_utilidad.value = 0;
   porcentaje_impuesto.value = 0;
   valor_venta.value = 0;
   ganancia.value = 0;
-};
+}
